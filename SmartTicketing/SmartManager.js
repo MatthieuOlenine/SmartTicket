428e109d090cef9a879b8c41e86ec6087968e18a
MatthieuOlenine <104694590+MatthieuOlenine@users.noreply.github.com>
2024-12-09 18:28:55 +0100

    Add files via upload
    first commit

---
+++ SmartTicketing/SmartManager.js
@@ -0,0 +1,144 @@
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, './SmartDB.json');
+
+class SmartManager {
+    constructor() {
+        this.fs = fs;
+        this.path = path;
+        this.filePath = filePath;
+        this.MaxEvent = 5;
+        this.Data = []
+        try {
+            const data = this.fs.readFileSync(this.filePath, 'utf8');
+            this.Data = JSON.parse(data);
+        } catch (error) {
+            console.error('\nErreur lors de la récupération du fichiers produits - code 11', error);
+        }
+    }
+
+    AddEvent(_name, _max = 5) {
+        for (let i of this.Data) {
+            if (i.name === _name) {
+                console.log('\nCe nom est déjà utilisé');                    
+                return(false)
+            }
+        }
+
+        if (this.Data.length >= this.MaxEvent) {
+            console.log('\nIl ne peut pas éxister plus de ', this.MaxEvent, 'évènements en même temps. Supprimez ceux éxistant pour en créer un nouveau');                    
+            return(false)
+        }
+
+        const NewEvent = {
+            type : "smart",
+            name : _name,
+            address : crypto.createHash('sha256').update(_name).digest('hex'),
+            horodatage : new Date().toISOString(),
+            max : _max,
+            registered : []
+        }
+
+        this.Data.push(NewEvent);
+        this.UpToDate()
+    }
+
+    DelEvent(_name) {
+        for (let i of this.Data) {
+            if (i.name === _name) {
+                this.Data = this.Data.filter(i => i.name !== _name);
+                console.log('\n',_name,'supprimé avec succès');
+            }
+        }
+
+        this.UpToDate()
+    }
+
+    GetEvent() {
+        return(this.Data)
+    }
+
+    AddRegistered(_eventname, _name) {
+        const NewRegistered = {
+            parameter : "admin",
+            name : _name,
+            address : crypto.createHash('sha256').update(_name).digest('hex'),
+            horodatage : new Date().toISOString()
+        }
+
+        for (let i of this.Data) {
+            if (i.name === _eventname) {
+
+                for (let j of i.registered) {
+                    if (j.name === _name) {
+                        console.log('\nCe nom est déjà utilisé');                    
+                        return(false)
+                    }
+                }
+                
+                if (i.registered.length >= i.max) {
+                    console.log('\nCet évènement ne peut pas accueillir plus de', i.max, 'personnes');                    
+                    return(false)
+                }
+
+                i.registered.push(NewRegistered);
+            }
+        }
+
+        this.UpToDate()
+    }
+
+    SetRegistered(_eventname, _parameter) {
+        for (let i of this.Data) {
+            if (i.name === _eventname) {
+                for (let j of i.registered) {
+                    j.parameter = _parameter
+                }
+            }
+        }
+
+        this.UpToDate()
+    }
+
+    DelRegistered(_eventname, _name) {
+        for (let i of this.Data) {
+            if (i.name === _eventname) {
+                for (let j of i.registered) {
+                    if (j.name === _name) {
+                        i.registered = i.registered.filter(i => i.name !== _name);
+                        this.Data.registered = i.registered;
+                        console.log('\n',_name,'supprimé avec succès');
+                    }
+                }
+            }
+        }
+
+        this.UpToDate()
+    }
+
+    LogIn(_eventname, _name) {
+        for (let i of this.Data) {
+            if (i.name === _eventname) {
+                for (let j of i.registered) {
+                    if (j.name === _name) {
+                        this.SetRegistered(_eventname, _name);
+                        return(true)
+                    }
+                }
+                return(false)
+            }
+        }
+    }
+
+    UpToDate() {
+        try {
+            this.fs.writeFileSync(this.filePath, JSON.stringify(this.Data, null, 2), 'utf8');
+            console.log('\nOpération enregistrés avec succès');
+        } catch (error) {
+            console.error('\nErreur lors de L\'enregistrement de l\'opération', error);
+        }
+    }
+}
+
+module.exports = SmartManager;
---
+++ app.js
@@ -0,0 +1,195 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 1623;
+const https = require('https');
+const fs = require('fs');
+
+//A ACTIVER POUR ENVOYER EN PRODUCTION !!!!!!
+/*
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/logiqservices.com/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/logiqservices.com/fullchain.pem')
+}
+*/
+app.use(cors()); 
+app.use(bodyParser.json()); // Pour analyser le JSON dans le corps des requêtes
+
+const BasicManager = require('./BasicTicketing/BasicManager.js');
+const DynamicManager = require('./DynamicTicketing/DynamicManager.js');
+const SmartManager = require('./SmartTicketing/SmartManager.js');
+
+function GenerateInstanceBasicManager(){
+    BasicInstance = new BasicManager();
+}
+
+function GenerateInstanceDynamicManager(){
+    DynamicInstance = new DynamicManager();
+}
+
+function GenerateInstanceSmartManager(){
+    SmartInstance = new SmartManager();
+}
+
+// API méthodes basique
+
+app.post('/API/BasicInstance/AddEvent', (req, res) => {
+    GenerateInstanceBasicManager();
+    const { _name, _max = 5 } = req.body;
+    const Data = BasicInstance.AddEvent(_name, _max);
+    BasicInstance = null;
+    res.send(Data);
+});
+
+app.post('/API/BasicInstance/DelEvent', (req, res) => {
+    GenerateInstanceBasicManager();
+    const { _name } = req.body;
+    const Data = BasicInstance.DelEvent(_name);
+    BasicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/BasicInstance/GetEvent', (req, res) => {
+    GenerateInstanceBasicManager();
+    const {} = req.body;
+    const Data = BasicInstance.GetEvent();
+    BasicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/BasicInstance/AddRegistered', (req, res) => {
+    GenerateInstanceBasicManager();
+    const { _eventname, _name } = req.body;
+    const Data = BasicInstance.AddRegistered(_eventname, _name);
+    BasicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/BasicInstance/DelRegistered', (req, res) => {
+    GenerateInstanceBasicManager();
+    const { _eventname, _name } = req.body;
+    const Data = BasicInstance.DelRegistered(_eventname, _name);
+    BasicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/BasicInstance/LogIn', (req, res) => {
+    GenerateInstanceBasicManager();
+    const {_eventname, _name} = req.body;
+    const Data = BasicInstance.LogIn(_eventname, _name);
+    BasicInstance = null;
+    res.send(Data);
+});
+
+// API méthodes dynamique
+
+app.post('/API/DynamicInstance/AddEvent', (req, res) => {
+    GenerateInstanceDynamicManager();
+    const { _name, _max = 5 } = req.body;
+    const Data = DynamicInstance.AddEvent(_name, _max);
+    DynamicInstance = null;
+    res.send(Data);
+});
+
+app.post('/API/DynamicInstance/DelEvent', (req, res) => {
+    GenerateInstanceDynamicManager();
+    const { _name } = req.body;
+    const Data = DynamicInstance.DelEvent(_name);
+    DynamicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/DynamicInstance/GetEvent', (req, res) => {
+    GenerateInstanceDynamicManager();
+    const {} = req.body;
+    const Data = DynamicInstance.GetEvent();
+    DynamicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/DynamicInstance/AddRegistered', (req, res) => {
+    GenerateInstanceDynamicManager();
+    const { _eventname, _name } = req.body;
+    const Data = DynamicInstance.AddRegistered(_eventname, _name);
+    DynamicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/DynamicInstance/DelRegistered', (req, res) => {
+    GenerateInstanceDynamicManager();
+    const { _eventname, _name } = req.body;
+    const Data = DynamicInstance.DelRegistered(_eventname, _name);
+    DynamicInstance = null;
+    res.send(Data);
+});
+
+app.post('API/DynamicInstance/LogIn', (req, res) => {
+    GenerateInstanceDynamicManager();
+    const {_eventname, _name} = req.body;
+    const Data = DynamicInstance.LogIn(_eventname, _name);
+    DynamicInstance = null;
+    res.send(Data);
+});
+
+// API méthodes intelligente
+
+app.post('/API/SmartInstance/AddEvent', (req, res) => {
+    GenerateInstanceSmartManager();
+    const { _name, _max = 5 } = req.body;
+    const Data = SmartInstance.AddEvent(_name, _max);
+    SmartInstance = null;
+    res.send(Data);
+});
+
+app.post('/API/SmartInstance/DelEvent', (req, res) => {
+    GenerateInstanceSmartManager();
+    const { _name } = req.body;
+    const Data = SmartInstance.DelEvent(_name);
+    SmartInstance = null;
+    res.send(Data);
+});
+
+app.post('API/SmartInstance/GetEvent', (req, res) => {
+    GenerateInstanceSmartManager();
+    const {} = req.body;
+    const Data = SmartInstance.GetEvent();
+    SmartInstance = null;
+    res.send(Data);
+});
+
+app.post('API/SmartInstance/AddRegistered', (req, res) => {
+    GenerateInstanceSmartManager();
+    const { _eventname, _name } = req.body;
+    const Data = SmartInstance.AddRegistered(_eventname, _name);
+    SmartInstance = null;
+    res.send(Data);
+});
+
+app.post('API/SmartInstance/DelRegistered', (req, res) => {
+    GenerateInstanceSmartManager();
+    const { _eventname, _name } = req.body;
+    const Data = SmartInstance.DelRegistered(_eventname, _name);
+    SmartInstance = null;
+    res.send(Data);
+});
+
+app.post('API/SmartInstance/LogIn', (req, res) => {
+    GenerateInstanceSmartManager();
+    const {_eventname, _name} = req.body;
+    const Data = SmartInstance.LogIn(_eventname, _name);
+    SmartInstance = null;
+    res.send(Data);
+});
+
+//A ACTIVER EN PRODUCTION !!!!
+/*
+https.createServer(options, app).listen(1623, () =>{
+    console.log('\n Server is running on port 1623 with https\n\nhttps://logiqservices.com:1623\n');
+})
+*/
+// A DESACTIVER AVANT DE BUILD !!!
+
+app.listen(port, () => {
+    console.log('app.js running on port 1623\n\nhttp://localhost:1623');
+});
